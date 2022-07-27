7ccbd066864c2240b8793ae2df125a32b1a98cf2
dsy4567 <dsy4567@outlook.com>
2022-07-27 17:05:47 +0800

    .

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-/**
+/*
 MIT License
 
 Copyright (c) 2022 dsy4567(https://github.com/dsy4567/ ; dsy4567)
@@ -20,8 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
- */
+*/
 
 const vscode = require("vscode");
 const cheerio = require("cheerio");
